1c63ced7293dfe2ce7e386fed2852cbdb47ed006
Pepe-Cecotto <peter.pieters@st-martinus.be>
2025-10-27 17:03:38 +0100

    Add files via upload

---
+++ sketch_feb07a.ino
@@ -0,0 +1,18 @@
+float vorigeTijd = 0;
+float interval = 1000;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+digitalWrite(13, HIGH);
+vorigeTijd = millis();
+}
+
+void loop() {
+
+if (millis()-vorigeTijd >= interval) {
+  digitalWrite(13, !digitalRead(13));
+  vorigeTijd = millis();
+}
+}
